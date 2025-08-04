b918abfdf35c03f8bb7a312db242977ec01b619b
LouieSF <lsf2004impo@gmail.com>
2025-08-04 14:37:41 -0400

    Completed basic Trie implementation of insert, search, and hasPrefix

--- trie_implementation/trie.cpp
+++ trie_implementation/trie.cpp
@@ -1,3 +1,43 @@
-
-
 #include "trie.h"
+
+
+void Trie::insert(string& word)
+{
+    TrieNode* current = root;
+    for (char ch : word)
+    {
+        if (current->children.find(ch) == current->children.end())
+        {
+            current->children[ch] = new TrieNode();
+        }
+    }
+    current->end_word = true;
+}
+
+bool Trie::search (string& word)
+{
+    TrieNode* current = root;
+    for (char ch : word)
+    {
+        if (current->children.find(ch) == current->children.end())
+        {
+            return false;
+        }
+        current = current->children[ch];
+    }
+    return (current != nullptr && current->end_word);
+}
+
+bool Trie::hasPrefix(string& prefix)
+{
+    TrieNode* current = root;
+    for (char ch : prefix)
+    {
+        if (current->children.find(ch) == current->children.end())
+        {
+            return false;
+        }
+        current = current->children[ch];
+    }
+    return true;
+}
--- trie_implementation/trie.h
+++ trie_implementation/trie.h
@@ -1,14 +1,48 @@
-
-
 #ifndef TRIE_H
 #define TRIE_H
 
+#include<string>
+#include<unordered_map>
+#include<vector>
+using namespace std;
 
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool end_word;
 
-class trie {
+    TrieNode() : end_word(false) {}
 
+    ~TrieNode()
+    {
+        for (auto& [key, value] : children)
+        {
+            delete value;
+        }
+    }
 };
 
+class Trie
+{
+private:
+    TrieNode* root;
+
+public:
+    Trie ()
+    {
+        root = new TrieNode();
+    }
+    ~Trie()
+    {
+        delete root;
+    }
+
+    void insert(string& word);
+
+    bool search(string& word);
+
+    bool hasPrefix(string& prefix);
+};
 
 
 #endif //TRIE_H
